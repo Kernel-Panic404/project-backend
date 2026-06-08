4d59dc8b6a0fb28f5e347ceba459808e5e326916
Gabriel Bustos Moreno <gbustosm@unal.edu.co>
2026-06-07 21:40:32 -0500

    feat: create authentication endpoints urls for RF-01

--- Mentora/usuarios/urls.py
+++ Mentora/usuarios/urls.py
@@ -1,8 +1,18 @@
-﻿from django.urls import path
-from .views import HolaMundoView, UsuarioListView, UsuarioDetailView
+from django.urls import path
+from .views import (
+    LoginView,
+    LogoutView,
+    UsuarioCreateView,
+    UsuarioListView,
+    UsuarioDetailView,
+    UsuarioDeleteView,
+)
 
 urlpatterns = [
-    path("hola-mundo/", HolaMundoView.as_view(), name="hola-mundo"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", UsuarioCreateView.as_view(), name="register"),
     path("", UsuarioListView.as_view(), name="usuario-list"),
     path("<int:usuario_id>/", UsuarioDetailView.as_view(), name="usuario-detail"),
+    path("<int:usuario_id>/delete/", UsuarioDeleteView.as_view(), name="usuario-delete"),
 ]
