19d56b0167bcf3b239a7fb2678b6330484f9336a
PABLO ROSALES <prosales@unal.edu.co>
2026-07-01 23:34:10 -0500

    fix(auth): ajustar autenticación JWT

--- Mentora/usuarios/authentication.py
+++ Mentora/usuarios/authentication.py
@@ -6,22 +6,31 @@ from .models import Usuario
 
 class CustomJWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        auth_header = request.headers.get('Authorization')
-        if not auth_header or not auth_header.startswith('Bearer '):
+
+        print("========== AUTH ==========")
+        print(request.headers)
+        print("==========================")
+
+        auth_header = request.headers.get("Authorization")
+
+        print("Authorization:", auth_header)
+
+        if not auth_header or not auth_header.startswith("Bearer "):
             return None
 
-        token = auth_header.split(' ')[1]
+        token = auth_header.split(" ")[1]
+
         payload = AuthService.validate_token(token)
 
         if not payload:
-            raise AuthenticationFailed('Invalid or expired token.')
+            raise AuthenticationFailed("Invalid or expired token.")
 
         try:
-            usuario = Usuario.objects.get(id=payload.get('user_id'))
+            usuario = Usuario.objects.get(id=payload["user_id"])
         except Usuario.DoesNotExist:
-            raise AuthenticationFailed('User not found.')
+            raise AuthenticationFailed("User not found.")
 
         if AuthService.is_token_revoked(usuario, token):
-            raise AuthenticationFailed('Token revoked.')
+            raise AuthenticationFailed("Token revoked.")
 
         return (usuario, token)
